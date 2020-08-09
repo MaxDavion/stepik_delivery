861a8f2029d0f84de1ba044f944ea2751ddc4895
Максим Кузнецов <m.kuznetsov@restream.rt.ru>
2020-08-09 16:07:17 +0300

    проектное задание 5

--- forms.py
+++ forms.py
@@ -6,8 +6,7 @@ from wtforms.validators import Length, Email, InputRequired, EqualTo
 class LoginForm(FlaskForm):
     title = StringField('Stepik delivery')
     subtitle = StringField('Войдите, чтобы управлять')
-    # email = StringField("Электропочта", [Email(message="Неверный формат email")])
-    email = StringField("Электропочта", [Length(min=6, message="Неверный формат email")])
+    email = StringField("Электропочта", [Email(message="Неверный формат email")])
     password = PasswordField("Пароль", [Length(min=6, message="Пароль должен содержать не менее 6и символов")])
     submit = SubmitField('Войти')
 
@@ -21,8 +20,7 @@ class RegisterForm(LoginForm):
 class OrderForm(FlaskForm):
     name = StringField('Ваше имя', [InputRequired(message="Поле обязательно для заполнения")])
     address = StringField('Адрес', [InputRequired(message="Поле обязательно для заполнения")])
-    # email = StringField("Электропочта", [Email(message="Неверный формат email")])
-    email = StringField("Электропочта", [Length(min=6, message="Неверный формат email")])
+    email = StringField("Электропочта", [Email(message="Неверный формат email")])
     phone = StringField("Телефон", [Length(min=7, message="Телефон должен содержать не менее 7 символов")])
     submit = SubmitField('Оформить заказ')
 
